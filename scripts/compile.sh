a1741f955b361f47702e6d1ed0b13fec151f3c68
mmcwilliams <hi@mmcwilliams.com>
2019-11-18 23:25:47 -0500

    Made first pass at progress on #2. Moved all nw.js code into electron renderer files and all third party libs into the dist/contrib directory. Copied the unused styling.css file into a styles.less file for later use. Build process is in place for typescript/less. Breaks on compile script ATM because I am still porting code and getting it into a working state with minimal changes in electron. Refactor is still TODO.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# styles
+./node_modules/.bin/lessc ./src/renderer/style.less ./dist/css/style.css
+
 # main process
 ./node_modules/.bin/tsc -p tsconfig.json --extendedDiagnostics
 
 # renderer process
-./node_modules/.bin/tsc ./src/renderer/index.ts --outFile ./dist/renderer/index.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
+./node_modules/.bin/tsc ./src/renderer/index.ts --outFile ./dist/renderer/index.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
