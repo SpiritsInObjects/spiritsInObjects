e23bf4eb4fdce4e4dd36429191049d5ef2c52c22
mmcwilliams <hi@mmcwilliams.com>
2019-11-18 19:24:37 -0500

    Quick re-structure of the source file structure. Now all source files, main and renderer, are in the src dir. Managing multiple typescript build processes is a pain, but at the project size right now this is managable for the moment.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,4 +4,4 @@
 ./node_modules/.bin/tsc -p tsconfig.json --extendedDiagnostics
 
 # renderer process
-./node_modules/.bin/tsc ./renderer/index.ts --outFile ./renderer/index.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
+./node_modules/.bin/tsc ./src/renderer/index.ts --outFile ./dist/renderer/index.js --noImplicitAny --lib ES2017 --lib ES2016 --lib dom -t ES2016
