e0a587cec05fa6c0d245c4d2e7f6301d7af79956
mmcwilliams <hi@mmcwilliams.com>
2019-11-20 17:25:54 -0500

    Mac build was failing because of case-sensitivity.

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -7,7 +7,7 @@ mkdir -p ./releases/mac
 #build dmg for mac install
 sleep 5s
 #--icon=assets/icons/icon.icns 
-./node_modules/.bin/electron-installer-dmg ./releases/mac/spiritsInObjects.app spiritsInObjects --out=./releases/mac  --overwrite    
+./node_modules/.bin/electron-installer-dmg ./releases/mac/spiritsinobjects.app spiritsinobjects --out=./releases/mac  --overwrite    
 # Path to the icon file that will be the app icon in the DMG window.
 #  --icon-size=<px>     How big to make the icon for the app in the DMG. [Default: `80`].
 #  --background=<path>  Path to a PNG image to use as the background of the DMG.
