ddfbb052bbd6b064598b5daa90a46b441c8b1968
mmcwilliams <hi@mmcwilliams.com>
2019-11-23 16:07:23 -0500

    Rename "scalar" variables to multipliers because that's what they are.

--- dist/renderer/sonifyCanvas.js
+++ dist/renderer/sonifyCanvas.js
@@ -2,11 +2,11 @@
 var framerate = 24;
 var samprate = 48000;
 var samplesPerFrame = samprate / framerate;
-const RED_SCALAR = 0.3;
-const GREEN_SCALAR = 0.59;
-const BLUE_SCALAR = 0.11;
+const RED_MULTIPLIER = 0.3;
+const GREEN_MULTIPLIER = 0.59;
+const BLUE_MULTIPLIER = 0.11;
 function sonifyCanvas(audioCtx, canvas, ctx) {
-    let heightScalar = (canvas.height / samplesPerFrame); //Ratio of sample size to height of frame
+    let heightMultiplier = (canvas.height / samplesPerFrame); //Ratio of sample size to height of frame
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height); //Get Uint8ClampedArray of pixel data from canvas
     let audioBuffer = audioCtx.createBuffer(1, samplesPerFrame, samprate); //Create AudioBuffer to support
     let monoBuffer = audioBuffer.getChannelData(0); //Get single channel buffer from AudioBuffer
@@ -18,9 +18,9 @@ function sonifyCanvas(audioCtx, canvas, ctx) {
     let fadeLengthInSamples = 30.0;
     let fadeIncrement = 1.0 / fadeLengthInSamples;
     for (let sample = 0; sample < audioBuffer.length; sample++) {
-        scaledStart = Math.floor(sample * heightScalar);
+        scaledStart = Math.floor(sample * heightMultiplier);
         scaledEnd = scaledStart + 1;
-        alpha = (sample * heightScalar) - scaledStart;
+        alpha = (sample * heightMultiplier) - scaledStart;
         if (sample < fadeLengthInSamples) {
             fadeMultiplier = sample * fadeIncrement;
         }
@@ -44,8 +44,8 @@ function getRowLuminance(data, width, scaledStart, scaledEnd, alpha) {
         // convert the RGB to HSL (we want L)
         if (i < locationOfSoundtrack)
             continue;
-        L1 = RED_SCALAR * data[scaledStart * width + i * 4] + GREEN_SCALAR * data[scaledStart * width + i * 4 + 1] + BLUE_SCALAR * data[scaledStart * width + i * 4 + 2];
-        L2 = RED_SCALAR * data[scaledEnd * width + i * 4] + GREEN_SCALAR * data[scaledEnd * width + i * 4 + 1] + BLUE_SCALAR * data[scaledEnd * width + i * 4 + 2];
+        L1 = RED_MULTIPLIER * data[scaledStart * width + i * 4] + GREEN_MULTIPLIER * data[scaledStart * width + i * 4 + 1] + BLUE_MULTIPLIER * data[scaledStart * width + i * 4 + 2];
+        L2 = RED_MULTIPLIER * data[scaledEnd * width + i * 4] + GREEN_MULTIPLIER * data[scaledEnd * width + i * 4 + 1] + BLUE_MULTIPLIER * data[scaledEnd * width + i * 4 + 2];
         luminance += ((1 - alpha) * L1 + alpha * L2) / 128.0 - 1.0;
     }
     luminance = luminance / (width / 4.0);
--- src/renderer/lib/sonifyCanvas/index.ts
+++ src/renderer/lib/sonifyCanvas/index.ts
@@ -4,12 +4,12 @@ var framerate = 24;
 var samprate = 48000;
 var samplesPerFrame = samprate/framerate;
 
-const RED_SCALAR : number = 0.3;
-const GREEN_SCALAR : number = 0.59;
-const BLUE_SCALAR : number = 0.11;
+const RED_MULTIPLIER : number = 0.3;
+const GREEN_MULTIPLIER : number = 0.59;
+const BLUE_MULTIPLIER : number = 0.11;
 
 function sonifyCanvas (audioCtx : AudioContext, canvas : HTMLCanvasElement, ctx : CanvasRenderingContext2D) : AudioBuffer {
-    let heightScalar : number = (canvas.height / samplesPerFrame);                          //Ratio of sample size to height of frame
+    let heightMultiplier : number = (canvas.height / samplesPerFrame);                          //Ratio of sample size to height of frame
     let imageData : ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);        //Get Uint8ClampedArray of pixel data from canvas
     let audioBuffer : AudioBuffer = audioCtx.createBuffer(1, samplesPerFrame, samprate);    //Create AudioBuffer to support
     let monoBuffer : Float32Array = audioBuffer.getChannelData(0);                          //Get single channel buffer from AudioBuffer
@@ -23,9 +23,9 @@ function sonifyCanvas (audioCtx : AudioContext, canvas : HTMLCanvasElement, ctx 
     let fadeIncrement = 1.0 / fadeLengthInSamples;
 
     for (let sample : number = 0; sample < audioBuffer.length; sample ++) {
-        scaledStart = Math.floor(sample * heightScalar);
+        scaledStart = Math.floor(sample * heightMultiplier);
         scaledEnd   = scaledStart + 1;
-        alpha = (sample * heightScalar) - scaledStart;
+        alpha = (sample * heightMultiplier) - scaledStart;
 
         if (sample < fadeLengthInSamples) {
             fadeMultiplier = sample * fadeIncrement;
@@ -50,8 +50,8 @@ function getRowLuminance (data : Uint8ClampedArray, width : number, scaledStart 
         // convert the RGB to HSL (we want L)
         if (i < locationOfSoundtrack) continue;
         
-        L1 = RED_SCALAR * data[scaledStart * width + i * 4] +  GREEN_SCALAR * data[scaledStart * width + i * 4 + 1] +  BLUE_SCALAR * data[scaledStart * width + i * 4 + 2];
-        L2  = RED_SCALAR * data[scaledEnd * width + i * 4] +  GREEN_SCALAR * data[scaledEnd * width + i * 4 + 1] + BLUE_SCALAR * data[scaledEnd * width + i * 4 + 2];
+        L1 = RED_MULTIPLIER * data[scaledStart * width + i * 4] +  GREEN_MULTIPLIER * data[scaledStart * width + i * 4 + 1] +  BLUE_MULTIPLIER * data[scaledStart * width + i * 4 + 2];
+        L2  = RED_MULTIPLIER * data[scaledEnd * width + i * 4] +  GREEN_MULTIPLIER * data[scaledEnd * width + i * 4 + 1] + BLUE_MULTIPLIER * data[scaledEnd * width + i * 4 + 2];
         luminance += ( (1 - alpha) * L1 + alpha * L2 )  / 128.0 - 1.0;
     }
     luminance = luminance / (width / 4.0);
