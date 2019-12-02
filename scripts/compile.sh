a1c91ca0934cd43bfdd17f8a70f54a39c0ea06f6
mmcwilliams <hi@mmcwilliams.com>
2019-12-02 09:54:27 -0500

    Restore index.js as script.js anticipating re-write using index.js for renderer code

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,7 +7,7 @@
 ./node_modules/.bin/tsc -p tsconfig.json --extendedDiagnostics
 
 # renderer process
-./node_modules/.bin/tsc ./src/renderer/index.ts --outFile ./dist/renderer/index.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
+./node_modules/.bin/tsc ./src/renderer/script.ts --outFile ./dist/renderer/script.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
 ./node_modules/.bin/tsc ./src/renderer/loadfiles.ts --outFile ./dist/renderer/loadfiles.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
 ./node_modules/.bin/tsc ./src/renderer/phillcorder.ts --outFile ./dist/renderer/phillcorder.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
 ./node_modules/.bin/tsc ./src/renderer/recorder.ts --outFile ./dist/renderer/recorder.js --noImplicitAny --lib ES2017 --lib ES2016 --lib ES2018 --lib dom -t ES2018
