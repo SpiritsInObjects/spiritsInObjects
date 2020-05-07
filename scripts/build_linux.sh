a6e286126f2c6a674a8284e271f7b70a082b3477
mmcwilliams <hi@mmcwilliams.com>
2020-05-07 11:14:16 -0400

    Fix build scripts to better support ffmpeg-static

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,6 +5,6 @@ mkdir -p ./releases/linux
 #package app
 #--icon=assets/icons/icon.png
 # for json root "icon": "./assets/icons/icon.png",
-./node_modules/.bin/electron-packager . spiritsinobjects --overwrite --asar --platform=linux --arch=x64  --prune=true --out=./releases/linux
+./node_modules/.bin/electron-packager . spiritsinobjects --overwrite --platform=linux --arch=x64  --prune=true --out=./releases/linux
 #build a .deb installer
 ./node_modules/.bin/electron-installer-debian --src ./releases/linux/spiritsinobjects-linux-x64/ --arch amd64 --config ./scripts/build_linux.json
--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -3,6 +3,6 @@
 mkdir -p ./releases
 mkdir -p ./releases/win
 
-./node_modules/.bin/electron-packager . spiritsInObjects --overwrite --asar --platform=win32 --arch=x64 --icon=assets/icons/icon.ico --prune=true --out=./releases/win --version-string.CompanyName="dartmouth.edu" --version-string.FileDescription="Application for image sonification and sequencing" --version-string.ProductName="spiritsInObjects"
+./node_modules/.bin/electron-packager . spiritsInObjects --overwrite --platform=win32 --arch=x64 --icon=assets/icons/icon.ico --prune=true --out=./releases/win --version-string.CompanyName="dartmouth.edu" --version-string.FileDescription="Application for image sonification and sequencing" --version-string.ProductName="spiritsInObjects"
 
 node ./scripts/build_win.js
