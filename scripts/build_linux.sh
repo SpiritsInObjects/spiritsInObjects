eede70a1f6a43464b3603f12b170b670aef3a3a6
sixteenmillimeter <matt@sixteenmillimeter.com>
2021-03-19 11:11:10 -0400

    This is so cool (it's not cool). I have to re-invent the build process to get a clean installer that is, as of latest build, half the size on linux. Expecting a massive reduction on mac as well.

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -4,11 +4,26 @@ version=$(jq -r  '.version' ./package.json)
 
 mkdir -p ./releases
 mkdir -p ./releases/linux
+mkdir -p ./build
+
+rm -rf ./build/dist
+
+cp -r ./dist ./build/
+cp ./package*.json ./build/
+
+cd build
+npm i
+
 #package app
 #--icon=assets/icons/icon.png
 # for json root "icon": "./assets/icons/icon.png",
-./node_modules/.bin/electron-packager . spiritsinobjects --overwrite --platform=linux --arch=x64  --prune=true --out=./releases/linux
+echo "Building application..."
+../node_modules/.bin/electron-packager . spiritsinobjects --overwrite --platform=linux --arch=x64  --prune=true --out=../releases/linux
 #build a .deb installer
-./node_modules/.bin/electron-installer-debian --src ./releases/linux/spiritsinobjects-linux-x64/ --arch amd64 --config ./scripts/build_linux.json
+echo "Creating debian installer..."
+../node_modules/.bin/electron-installer-debian --src ../releases/linux/spiritsinobjects-linux-x64/ --arch amd64 --config ../scripts/build_linux.json
+
+cd ..
+rm -r ./build
 
 echo "Built installer of version ${version}"
