a03212fe225d22fb87d2aa96a43b2a1c0a869263
sixteenmillimeter <matt@sixteenmillimeter.com>
2021-11-09 11:52:59 -0500

    Increment build number

--- scripts/docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./docs/code
-mkdir -p ./docs/code/sonifyCanvas
-mkdir -p ./docs/code/video
-
-./node_modules/.bin/jsdoc2md dist/renderer/lib/sonifyCanvas/index.js > ./docs/code/sonifyCanvas/Readme.md
-./node_modules/.bin/jsdoc2md dist/renderer/lib/video/index.js > ./docs/code/video/Readme.md
